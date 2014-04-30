98be0e79a9befa77a2a9763befede815f9921563
Jesse <jesserbyars@gmail.com>
2014-04-29 19:36:37 -0700

    4/29/2014
    Added Lama Image, added bottom border to header, added function to
    randomly change the image

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Cigarette</title>
+	<title>Smoking Saves Lives</title>
 	<link rel="stylesheet" href="styles/base.css">
 	<script src="scripts/jquery-1.11.0.min.js"></script>
 	<script src="scripts/jquery.color-2.1.2.min.js"></script>
@@ -17,6 +17,8 @@
 	<div id="cigdiv">
 		<?php 
 			create_cigarette();
+			$n = mt_rand(1, 10);
+			$second_picture = $n>7 ? "nurse.jpg" : "llama.jpg";
 		?>
 	</div>
 
@@ -35,7 +37,7 @@
 
 			<p>Smoking cigarettes has been proven to reduce stress in people who smoke. A "smoker" can suffer from increased tension and irritability due to the lack of toxic chemicals in their system. Upon inhaling the fumes from a cigarette the lungs begin to adsorb much need toxins into the blood stream and the body begins to balance. This effect can most often be felt during the first cigarette of the morning, or after and especially long breaks between smoking sessions. 7 out of 4 smokers reported this event reduces frustration and irritability.</p>
 
-			<p><img src="img/nurse.jpg" class="nurseimg">Smoking cigarettes is a universal social normalizer. Any and all smoking areas belong to the community; you may often find a vagrant socializing with a millionaire within the confines of a "designated smoking area." They share lighters and discuss current affairs. Their individual backgrounds mean little when they huddle together during winter to stay warm. This open acceptance is the standard among all smokers.  All other cultures and groups should take a page from the smoker's handbook.Groups seeking acceptance need only take up smoking and walk to any smoking area to find a warm welcome to the community. The nerds of Lamda Lamda Lamda only needed to hit the local Minute Mart, pick up a pack of camels and a Bic lighter, and the Alpha Betas would have welcomed them into the smoking shack at Adams College with open arms.</p>
+			<p><img src="img/<?php echo $second_picture?>" class="nurseimg">Smoking cigarettes is a universal social normalizer. Any and all smoking areas belong to the community; you may often find a vagrant socializing with a millionaire within the confines of a "designated smoking area." They share lighters and discuss current affairs. Their individual backgrounds mean little when they huddle together during winter to stay warm. This open acceptance is the standard among all smokers.  All other cultures and groups should take a page from the smoker's handbook.Groups seeking acceptance need only take up smoking and walk to any smoking area to find a warm welcome to the community. The nerds of Lamda Lamda Lamda only needed to hit the local Minute Mart, pick up a pack of camels and a Bic lighter, and the Alpha Betas would have welcomed them into the smoking shack at Adams College with open arms.</p>
 			
 			
 
